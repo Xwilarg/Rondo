bd988a345fd2c7a4818739e0d6d91fe3ab74d52d
Xwilarg <xwilarg@yahoo.fr>
2021-06-12 01:52:54 +0200

    Add empty Discord bot

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+DiscordSocketClient _client = new(new()
+{
+    LogLevel = LogSeverity.Verbose
+});
+
+_client.Log += (e) =>
+{
+    Console.WriteLine(e);
+    return Task.CompletedTask;
+};
+
+await _client.LoginAsync(TokenType.Bot, File.ReadAllText("token.txt"));
+await _client.StartAsync();
+
+await Task.Delay(-1);
